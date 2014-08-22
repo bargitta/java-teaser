e642494f0b49ae5741e0df3c5bdfd54802c0b474
bargitta <bargittachen@gmail.com>
2014-08-22 08:43:06 +0800

    implement qstack with tests

--- src/com/bargittachen/QStack.java
+++ src/com/bargittachen/QStack.java
@@ -7,14 +7,29 @@ import java.util.LinkedList;
  * Implement a stack using two queues
  */
 public class QStack<T> {
-    public void push(T element){
-        if(element != null){
 
+    public boolean push(T element){
+        if(element != null){
+            boolean success = queue1.isEmpty() ? queue2.offer(element) : queue1.offer(element);
+            return success;
         }
+        return false;
     }
 
     public T pop(){
-        return null;
+        if(queue1.isEmpty() && queue2.isEmpty())
+            return null;
+        if(queue1.isEmpty()){
+            while(queue2.size() > 1){
+                queue1.offer(queue2.poll());
+            }
+            return  queue2.poll();
+        }else{
+            while(queue1.size() > 1){
+                queue2.offer(queue1.poll());
+            }
+            return  queue1.poll();
+        }
     }
 
     private Queue<T> queue1 = new LinkedList<T>();
--- src/test/QStackTest.java
+++ src/test/QStackTest.java
@@ -1,7 +1,36 @@
 package test;
+import com.bargittachen.QStack;
+import org.junit.Assert;
+import org.junit.Test;
 
-/**
- * Created by xichen on 8/22/14.
- */
+import static org.junit.Assert.*;
+
 public class QStackTest {
+    @Test
+    public void push_pop_one_element_with_qstack(){
+        QStack<String> strStack = new QStack<String>();
+        assertTrue(strStack.push("a"));
+        assertEquals("a", strStack.pop());
+    }
+
+    @Test
+    public void push_pop_invalid_elements_with_qstack(){
+        QStack<String> strStack = new QStack<String>();
+        assertFalse(strStack.push(null));
+        assertNull(strStack.pop());
+    }
+
+    @Test
+    public void push_pop_elements_with_qstack(){
+        QStack<String> strStack = new QStack<String>();
+        assertTrue(strStack.push("a"));
+        assertTrue(strStack.push("b"));
+        assertTrue(strStack.push("c"));
+        assertEquals("c", strStack.pop());
+        assertEquals("b", strStack.pop());
+        assertEquals("a", strStack.pop());
+        assertNull(strStack.pop());
+
+    }
+
 }
